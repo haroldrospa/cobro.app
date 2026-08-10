c6e559bb15c32fcb4e9064a706676d097fd94bdb
Harold <haroldrospa@users.noreply.github.com>
2026-08-10 19:31:07 -0400

    fix: remover plugin-legacy para acelerar build y corregir error de desplegado en Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { VitePWA } from 'vite-plugin-pwa';
-import legacy from '@vitejs/plugin-legacy';
 import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
@@ -34,9 +33,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
     viteCompression({
       algorithm: 'brotliCompress',
       ext: '.br',
